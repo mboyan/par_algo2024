3cb64e670560428fa05cafffc081d32eafffa70b
Boyan Mihaylov <mih.boyan@gmail.com>
2024-10-03 15:37:17 +0200

    Update main_hello.f90

--- main_hello.f90
+++ main_hello.f90
@@ -6,12 +6,6 @@ program hello
     ! It does not work -- the output order is left to the process management of the
     ! cluster (SLURM in this case), and on DelftBlue it is always serialized at the end
 
-    integer :: me, n
-
-    ! Get the number of images and the current image index
-    n = num_images()
-    me = this_image()
-
     write(*,'(A,I2,A,I2)') 'Hello from image ',this_image(),'out of ',num_images()
     sync all
     write(*,'(A,I2,A,I2)') 'Goodbye from image ',this_image(),'out of ',num_images()
